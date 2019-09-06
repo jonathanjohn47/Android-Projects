260b066d5d96725b2589793d9d0104c1195bfabe
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-09-06 18:35:23 +0530

    Toggle Button in Java
    A simple Java implementation of a Toggle button in Android. The app plays 'Peek a boo' upon pressing the toggle button.

---
+++ ToggleButton.java
@@ -0,0 +1,38 @@
+package com.example.togglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = (ToggleButton)findViewById(R.id.toggleButton);
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setVisibility(View.INVISIBLE);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setText("Peek a Boo!");
+                }
+                else{
+                    textView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
