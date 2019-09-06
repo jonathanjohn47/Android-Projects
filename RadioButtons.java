cce235f9452f3a6b134e33863174c0b3dd65c783
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-09-06 12:17:03 +0530

    Radio Buttons in Java
    A sample java code implementing Radio Buttons in radio group and its interaction in an Android App

--- RadioButtons.java
+++ RadioButtons.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
+        /*textView = (TextView)findViewById(R.id.textView);
         radioGroup = (RadioGroup)findViewById(R.id.radiogroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -26,22 +27,38 @@ public class MainActivity extends AppCompatActivity {
                 radioButton = (RadioButton)findViewById(checkedId);
                 switch (radioButton.getId()){
                     case R.id.rbyes: {
-                        /*Do something when "Yes" radio button is clicked*/
+                        //Do something when "Yes" radio button is clicked
                         textView.setText("Yes");
                     }
                     break;
                     case R.id.rbmaybe: {
-                        /*Do something when "Maybe" radio button is clicked*/
+                        //Do something when "Maybe" radio button is clicked
                         textView.setText("Maybe");
                     }
                     break;
                     case R.id.rbno: {
-                        /*Do something when "No" radio button is clicked*/
+                        //Do something when "No" radio button is clicked
                         textView.setText("No");
                     }
                     break;
                 }
             }
-        });
+        });*/
+    }
+
+    /*There is another method to detect radio button being checked is by assigning an "OnCLick" to each button,
+    * and then creating separate methods/fucntions in java leading to those onclicks*/
+
+    public void yesclick(View v){
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Yes");
+    }
+    public void maybeclick(View v) {
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Maybe");
+    }
+    public void noclick(View v){
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText("No");
     }
 }
