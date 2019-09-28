27b16daf4f8b9c10e7b5026d440f9280d14930dd
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-09-28 20:29:51 +0530

    Create MainActivity.java

---
+++ Recycler View/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.myrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        for (int i=0; i<100; i++){
+            ListItem listitem = new ListItem("First Name " + i, "Last Name " + i, i);
+            listItems.add(listitem);
+        }
+
+        adapter = new Adapter(listItems, this);
+        recyclerView.setAdapter(adapter);
+    }
+}
