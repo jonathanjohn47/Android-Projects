56a4b5206636be2770753b5736115975cf29dd61
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-12-01 10:45:25 +0530

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,19 @@
+class MyMain {
+	public static void main( String [] args ){
+		int a = 10, b = 20;
+		int c = a + b;
+		System.out.println("c = " + c );
+
+		c = (int) 3.14;
+
+		if( c == 10 ) {
+
+		}
+	}
+}
+
+class YourMain {
+	public static void main( String [] args ){
+		System.out.println("Hello Java!");
+	}
+}
