370ac87421e4a2d5e0bd9fbb6df48c873b992b0d
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2020-01-23 11:28:37 +0530

    Create DatePickerDialog.java

---
+++ Miscellaneous/DatePickerDialog.java
@@ -0,0 +1,21 @@
+@Override
+public void onClick(View view) {
+    DatePickerDialog datePickerDialog = new DatePickerDialog(StudentsLeaveRequests.this);
+    datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            String date_s = i + "-" + (i1+1) + "-" + i2;
+            SimpleDateFormat dt = new SimpleDateFormat("yyyy-M-d");
+            Date date;
+            try {
+                date = dt.parse(date_s);
+                SimpleDateFormat dt1 = new SimpleDateFormat("dd-MMM-yyyy");
+                mSelectDate.setText(dt1.format(date));
+            } catch (ParseException e) {
+                Log.e("error", e+"");
+            }
+
+        }
+    });
+    datePickerDialog.show();
+}
