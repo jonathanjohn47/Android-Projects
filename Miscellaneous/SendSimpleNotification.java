2a01b6b1a656eefd0a59b6011d1b612962f00714
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2020-02-24 12:29:24 +0530

    Update SendSimpleNotification.java

--- Miscellaneous/SendSimpleNotification.java
+++ Miscellaneous/SendSimpleNotification.java
@@ -1,10 +1,19 @@
- private void sendNotification() {
+private void sendNotification() {
         Log.e("TAG", "Entered sendNotification() method");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setContentTitle("ALERT!!!");
         builder.setContentText("Some suspicious activity is detected. Please take quick action");
         builder.setPriority(NotificationManager.IMPORTANCE_HIGH);
+        builder.setAutoCancel(true);
+
+        //To open particular activity after user clicks on the notification
+        Intent notificationIntent = new Intent(mContext, MainActivity.class);
+        notificationIntent.putExtra("Alert", 1);
+        PendingIntent contentIntent = PendingIntent.getActivity(mContext, 1, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        //-----------------------------------------------------------------
 
         NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
