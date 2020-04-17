bfb07b4fc4bed5276fdc253d5285d516c0144592
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2020-04-17 19:28:10 +0530

    Create RuntimePermission.java

---
+++ Miscellaneous/RuntimePermission.java
@@ -0,0 +1,6 @@
+private void askPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+    }
